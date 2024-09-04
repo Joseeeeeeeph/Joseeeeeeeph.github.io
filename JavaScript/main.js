bc6bfc5e714915b8c4a335f62ee589b214488669
Joseph Adams <josephadams2@virginmedia.com>
2024-09-04 20:44:21 +0100

    Mobile button test

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -2,10 +2,10 @@ function setStyleSheet(url) {
     var stylesheet = document.getElementById('stylesheet');
     stylesheet.setAttribute('href', url);
     localStorage.setItem('theme', url);
-    if (url === '../CSS/dark.css' || url === '../CSS/hacker.css') {
-       document.getElementById('blur').src='../Resources/covblurdark.png';
+    if (url === 'CSS/dark.css' || url === 'CSS/hacker.css') {
+       document.getElementById('blur').src='Resources/covblurdark.png';
     } else {
-        document.getElementById('blur').src='../Resources/covblurlight.png';
+        document.getElementById('blur').src='Resources/covblurlight.png';
     }
 }
 
@@ -13,5 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var savedTheme = localStorage.getItem('theme');
     if (savedTheme) {
         setStyleSheet(savedTheme);
+    }
+});
+
+document.getElementById('toggle-nav').addEventListener('click', function() {
+    var nav = document.querySelector('nav');
+    if (nav.style.display === 'none' || nav.style.display === '') {
+        nav.style.display = 'block';
+    } else {
+        nav.style.display = 'none';
     }
 });
